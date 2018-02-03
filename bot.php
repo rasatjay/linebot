c0be95999ed944c9e7eec197dc16efda6d127be8
rasatjay <rasatjay@gmail.com>
2018-02-04 00:15:15 +0900

    modified add user reply

--- bot.php
+++ bot.php
@@ -13,13 +13,14 @@
             if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
                 // Get text sent
                 $text = $event['message']['text'];
+                $user = $event['source']['userId'];
                 // Get replyToken
                 $replyToken = $event['replyToken'];
                 
                 // Build message to reply back
                 $messages = [
                 'type' => 'text',
-                'text' => $text
+                'text' => $text+$user
                 ];
                 
                 // Make a POST Request to Messaging API to reply to sender
