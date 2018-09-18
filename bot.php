17206a2bc8e13a2fc8677fce783fab0faceee772
rasatjay <rasatjay@gmail.com>
2018-09-18 19:58:20 +0900

    a

--- bot.php
+++ bot.php
@@ -56,26 +56,39 @@
                             'type' => 'text',
                             'text' => "พิมพ์ จำนวนเงินบาทตามด้วย'thb' เพื่อแปลงเป็นเงินเยน"
                         ]
+                    
+                    
                         ,[
-                            'type'=> 'template',
-                            'altText'=> 'this is a confirm template',
-                            'template'=> [
-                                'type'=> 'confirm',
-                                'text'=> 'Are you sure?',
-                                'actions'=> [
-                                    [
-                                        'type'=> 'message',
-                                        'labe'=> 'Yes',
-                                        'text'=> 'yes'
-                                    ],
-                                    [
-                                        'type'=> 'message',
-                                        'label'=> 'No',
-                                        'text'=> 'no'
-                                    ]
-                                ]
-                            ]
+                            'type': 'location',
+                            'title': 'my location',
+                            'address': '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
+                            'latitude': 35.65910807942215,
+                            'longitude': 139.70372892916203
                         ]
+                    
+                    
+                    
+                    
+   //                     ,[
+   //                         'type'=> 'template',
+   //                        'altText'=> 'this is a confirm template',
+   //                         'template'=> [
+   //                             'type'=> 'confirm',
+   //                             'text'=> 'Are you sure?',
+   //                             'actions'=> [
+   //                                 [
+   //                                     'type'=> 'message',
+   //                                     'labe'=> 'Yes',
+   //                                     'text'=> 'yes'
+   //                                 ],
+   //                                 [
+   //                                     'type'=> 'message',
+   //                                     'label'=> 'No',
+   //                                     'text'=> 'no'
+   //                                 ]
+   //                             ]
+   //                         ]
+   //                     ]
                     
                     ];
                 }
