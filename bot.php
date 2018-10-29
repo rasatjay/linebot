0083a9b6a0794097a35b1cb6a91e14807eb31c30
rasatjay <rasatjay@gmail.com>
2018-10-29 21:15:47 +0700

    a

--- bot.php
+++ bot.php
@@ -65,13 +65,49 @@
                             'latitude'=> 35.65910807942215,
                             'longitude'=> 139.70372892916203
                         ]
-                        ,[
-                            'type'=> 'location',
-                            'title'=> 'my location',
-                        'address'=> '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
-                        'latitude'=> 35.65910807942215,
-                        'longitude'=> 139.70372892916203
-                        ]
+                    ,array (
+                            'type' => 'flex',
+                            'altText' => 'Flex Message',
+                            'contents' =>
+                            array (
+                                   'type' => 'carousel',
+                                   'contents' =>
+                                   array (
+                                          0 =>
+                                          array (
+                                                 'type' => 'bubble',
+                                                 'body' =>
+                                                 array (
+                                                        'type' => 'box',
+                                                        'layout' => 'vertical',
+                                                        'spacing' => 'sm',
+                                                        'contents' =>
+                                                        array (
+                                                               0 =>
+                                                               array (
+                                                                      'type' => 'button',
+                                                                      'action' =>
+                                                                      array (
+                                                                             'type' => 'uri',
+                                                                             'label' => 'See more',
+                                                                             'uri' => 'https://linecorp.com',
+                                                                             ),
+                                                                      'flex' => 1,
+                                                                      'gravity' => 'center',
+                                                                      ),
+                                                               ),
+                                                        ),
+                                                 ),
+                                          ),
+                                   ),
+                            )
+                    
+                    
+                    
+                    
+                    
+                    
+                    
                     
                     
                     
